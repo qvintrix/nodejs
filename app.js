66a661042a6fb146dea27b12a15e6f5f1269feac
Oleksii_Korkh <Oleksii_Korkh@epam.com>
2018-04-27 10:11:12 +0300

    fixes

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ import * as config from './config/application.json';
 import { User, Product } from './models';
 
 console.log(config.name);
-console.log(new User());
-console.log(new Product());
+new User();
+new Product();
