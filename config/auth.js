74864c2b159fea57c1cbe72c9e2f46e6b8f685d6
Oleksii_Korkh <oleksii_korkh@epam.com>
2018-06-19 10:12:36 +0300

    .

--- config/auth.js
+++ config/auth.js
@@ -1,22 +1,21 @@
 module.exports = {
+  secretKey: "secret",
+  facebookAuth: {
+    clientID: "217073305770482",
+    clientSecret: "9273e90b367d1e39fee22af1d4c10ff8",
+    callbackURL: "http://localhost:8080/auth/facebook/callback"
+  },
 
-	secretKey: "secret",
-	facebookAuth: {
-		clientID: '217073305770482',
-		clientSecret: '9273e90b367d1e39fee22af1d4c10ff8',
-		callbackURL: 'http://localhost:8080/auth/facebook/callback'
-	},
+  twitterAuth: {
+    consumerKey: "BpnRXi5EVQe8eSwZoMFfa64gt",
+    consumerSecret: "kSXNQ49rTRnFlj50VPb60e3kdFAOhiP91MIHQEeFnplKDsfzKR",
+    callbackURL: "http://localhost:8080/auth/twitter/callback"
+  },
 
-	twitterAuth: {
-		consumerKey: 'BpnRXi5EVQe8eSwZoMFfa64gt',
-		consumerSecret: 'kSXNQ49rTRnFlj50VPb60e3kdFAOhiP91MIHQEeFnplKDsfzKR',
-		callbackURL: 'http://localhost:8080'
-	},
-
-	googleAuth: {
-		clientID: '809261840706-rn3auakdm1vle72csmfoha4pe1g58fi9.apps.googleusercontent.com',
-		clientSecret: 'OmqhbugSWwG8T6xIh-b7aQUz',
-		callbackURL: 'http://localhost:8080/auth/google/callback'
-	}
-
+  googleAuth: {
+    clientID:
+      "809261840706-rn3auakdm1vle72csmfoha4pe1g58fi9.apps.googleusercontent.com",
+    clientSecret: "OmqhbugSWwG8T6xIh-b7aQUz",
+    callbackURL: "http://localhost:8080/auth/google/callback"
+  }
 };
